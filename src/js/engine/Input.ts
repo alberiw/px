17ff049f072ca1209cb8a45918d53234018069d1
Albert Wojtasik <albert.wojtasik@unity.pl>
2022-04-21 08:55:50 +0200

    improved axes

--- src/js/engine/Input.ts
+++ src/js/engine/Input.ts
@@ -3,52 +3,48 @@ import { KeyCode } from "./KeyCode";
 export class Input {
 
     private static _keys: number[] = [];
-    private static _axes: Map<string, number> = new Map();
+    private static _axes: Map<string, [KeyCode, KeyCode]> = new Map([
+        ["x", [KeyCode.ArrowLeft, KeyCode.ArrowRight]],
+        ["y", [KeyCode.ArrowUp, KeyCode.ArrowDown]],
+    ]);
 
     static get keys(): number[] {
         return this._keys;
     }
 
-    static getAxis(key: string): number {
-        return this._axes.get(key) ?? 0;
+    static getAxis(name: string): number {
+        const [key1, key2] = this._axes.get(name) ?? [null, null];
+        return this._calcAxis(key1, key2);
+    }
+
+    static setAxis(name: string, key1: KeyCode, key2: KeyCode) {
+        this._axes.set(name, [key1, key2]);
     }
 
     //TODO change keyCode to key ?? code ?? keyCode
     //https://devstephen.medium.com/keyboardevent-key-for-cross-browser-key-press-check-61dbad0a067a
     private static _init = (() => {
         window.onkeydown = (e: KeyboardEvent) => {
-            console.log(`${e.keyCode} - ${e.key} - ${e.code}`);
+            console.debug(`${e.keyCode} - ${e.key} - ${e.code}`);
             const key = e.keyCode ?? e.which;
             Input._keys.push(key);
-            Input._updateAxes();
-            
         };
         
         window.onkeyup = (e: KeyboardEvent) => {
             const key = e.keyCode ?? e.which;
             Input._keys = Input._keys.filter(e => e !== key);
-            Input._updateAxes();
         };
     })();
 
-    private static _updateAxes() {
-        let x = 0
-        if (Input._keys.includes(KeyCode.ArrowLeft)) {
-            x -= 1;
+    private static _calcAxis(key1: KeyCode, key2: KeyCode): number {
+        let axis = 0
+        if (Input._keys.includes(key1)) {
+            axis -= 1;
         }
-        if (Input._keys.includes(KeyCode.ArrowRight)) {
-            x += 1;
+        if (Input._keys.includes(key2)) {
+            axis += 1;
         }
-        Input._axes.set("x", x);
-        
-        let y = 0
-        if (Input._keys.includes(KeyCode.ArrowUp)) {
-            y -= 1;
-        }
-        if (Input._keys.includes(KeyCode.ArrowDown)) {
-            y += 1;
-        }
-        Input._axes.set("y", y)
+        return axis;
     }
 
 }
